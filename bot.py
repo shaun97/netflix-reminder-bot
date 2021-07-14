67827f587cc670c1504583005f95d3cb323eae8f
shaun <xshaun97@gmail.com>
2021-07-15 02:13:10 +0800

    typo in reminder function

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("reminder", reminder))
+    dp.add_handler(CommandHandler("reminder", register_reminder))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
