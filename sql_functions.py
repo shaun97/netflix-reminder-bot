f290fb5a8fa02211ca922d635c8ed0308d138f81
shaun <xshaun97@gmail.com>
2021-07-15 03:26:16 +0800

    change the table to insert to

--- sql_functions.py
+++ sql_functions.py
@@ -72,7 +72,7 @@ def set_admin(conn, user_id):
     print(f"setting {user_id} as admin")
 
     command = f"""
-        INSERT INTO users (user_id)
+        INSERT INTO settings (user_id)
         VALUES ('{user_id})';
     """
 
@@ -83,3 +83,20 @@ def set_admin(conn, user_id):
         conn.commit()
     except psycopg2.Error as e:
         conn.rollback()
+
+def check_if_admin(conn, user_id):
+    command = f"""
+        SELECT *
+        FROM settings
+        WHERE user_id='{user_id}';
+    """
+    cur = conn.cursor()
+    try:
+        cur.execute(command)
+        cur.close()
+        conn.commit()
+        print("success")
+        return True
+    except psycopg2.Error as e:
+        conn.rollback()
+        return False
